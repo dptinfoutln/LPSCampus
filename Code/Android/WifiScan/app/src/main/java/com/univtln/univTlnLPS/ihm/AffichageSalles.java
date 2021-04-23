5609207369abd38a66784e9bfca52d4cd20122e5
Tiffany83140 <tiffanyc83@live.fr>
2021-04-23 09:48:16 +0200

    ajout du code android

---
+++ Code/Android/WifiScan/app/src/main/java/com/univtln/univTlnLPS/ihm/AffichageSalles.java
@@ -0,0 +1,39 @@
+package com.univtln.univTlnLPS.ihm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import com.univtln.univTlnLPS.R;
+import com.univtln.univTlnLPS.ihm.Adapter.AdapterSalles;
+
+import java.util.ArrayList;
+
+public class AffichageSalles extends AppCompatActivity {
+
+    ListView ListeSalles;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_affichage_salles);
+    }
+
+
+    public void affichageSalle(View v) {
+        ListAdapter listeAdapter = new AdapterSalles(this, new ArrayList<>());
+        ListeSalles = (ListView) findViewById(R.id.listedonneessalle);
+        // remplir la listeAdapter avec la liste des salles grace à la BDD Java
+        // donner à la liste view la liste adpater
+        // rendre la liste view cliquable
+    }
+
+    public void onClickChoisir(View v) {
+        Intent i = new Intent(this, AffichageDonneesSalle.class);
+        startActivity(i);
+    }
+}
