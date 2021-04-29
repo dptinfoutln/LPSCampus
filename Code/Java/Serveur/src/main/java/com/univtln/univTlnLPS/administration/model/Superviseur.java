333b4eaf9dd0b221cb9ed0c6ced664b06d2d243e
lea <lea-glandus@etud.univ-tln.fr>
2021-04-29 10:50:19 +0200

    ajout password setter hashing

--- Code/Java/Serveur/src/main/java/com/univtln/univTlnLPS/administration/model/Superviseur.java
+++ Code/Java/Serveur/src/main/java/com/univtln/univTlnLPS/administration/model/Superviseur.java
@@ -5,15 +5,19 @@ import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
 import jakarta.validation.constraints.NotNull;
 import jakarta.xml.bind.annotation.XmlElement;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import lombok.experimental.SuperBuilder;
 
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.util.Arrays;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -34,4 +38,25 @@ public class Superviseur extends Utilisateur {
     @XmlElement
     @NotNull
     private String passwordHash;
+
+    @XmlElement
+    @NotNull
+    byte[] salt = new byte[16];
+
+    SecureRandom random = new SecureRandom();
+
+    public void setPasswordHash(String password) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        random.nextBytes(salt);
+        KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 65536, 128);
+        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+        this.passwordHash = new String(factory.generateSecret(spec).getEncoded());
+    }
+
+    @SneakyThrows
+    public boolean checkPassword(String password) {
+        KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 65536, 128);
+        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+        byte[] submittedPasswordHash = factory.generateSecret(spec).getEncoded();
+        return Arrays.equals(passwordHash.getBytes(), submittedPasswordHash);
+    }
 }
