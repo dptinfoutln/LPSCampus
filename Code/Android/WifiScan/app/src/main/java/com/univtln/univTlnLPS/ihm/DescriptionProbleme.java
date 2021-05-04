158e376241d0ae9886a1f65fe5218122c5ed873a
Tiffany83140 <tiffanyc83@live.fr>
2021-05-04 15:20:12 +0200

    fonctionnalité reporter problème okay

--- Code/Android/WifiScan/app/src/main/java/com/univtln/univTlnLPS/ihm/DescriptionProbleme.java
+++ Code/Android/WifiScan/app/src/main/java/com/univtln/univTlnLPS/ihm/DescriptionProbleme.java
@@ -2,21 +2,61 @@ package com.univtln.univTlnLPS.ihm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
 import com.univtln.univTlnLPS.R;
+import com.univtln.univTlnLPS.client.SSGBDControleur;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class DescriptionProbleme extends AppCompatActivity {
+
+    private EditText description;
+    private String pb;
+    private SSGBDControleur ssgbdControleur;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_description_probleme);
+
+        description = findViewById(R.id.description);
+
+        Intent i = getIntent();
+        ssgbdControleur = (SSGBDControleur)i.getSerializableExtra("ssgbdC");
+        String pb = i.getStringExtra("radioGroupId");
     }
 
-    public void validerpb(View v) {
-        // recuperer selection radiobutton
-        // envoyer à la bdd le probleme avec la description
+    public void validerpb(View v) throws JSONException {
+        JSONObject pid = new JSONObject();
+
+        pid.put("nom", pb);
+        pid.put("description", description.getText().toString());
+
+        String desc = description.getText().toString();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    ssgbdControleur.doRequest("PUT", "problemes", pid, !true);
+
+                    DescriptionProbleme.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            finish();
+                        }
+                    });
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 }
--- Code/Android/WifiScan/app/src/main/java/com/univtln/univTlnLPS/ihm/ReporterProbleme.java
+++ Code/Android/WifiScan/app/src/main/java/com/univtln/univTlnLPS/ihm/ReporterProbleme.java
@@ -5,12 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.univtln.univTlnLPS.R;
 import com.univtln.univTlnLPS.client.SSGBDControleur;
 
 public class ReporterProbleme extends AppCompatActivity {
 
+    private RadioGroup radioGroup;
     private SSGBDControleur ssgbdControleur;
 
     @Override
@@ -18,16 +21,17 @@ public class ReporterProbleme extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reporter_probleme);
 
-        // recuperer la selection
+        radioGroup = findViewById(R.id.radiogroup);
 
         Intent i = getIntent();
         ssgbdControleur = (SSGBDControleur)i.getSerializableExtra("ssgbdC");
     }
 
     public void valider(View v) {
-        // seulement si quelque chose est coché
+        String radiovalue = ((RadioButton)findViewById(radioGroup.getCheckedRadioButtonId())).getText().toString();
         Intent i = new Intent(this, DescriptionProbleme.class);
         i.putExtra("ssgbdC", ssgbdControleur);
+        i.putExtra("radioGroupId", radiovalue);
         startActivity(i);
 
     }
