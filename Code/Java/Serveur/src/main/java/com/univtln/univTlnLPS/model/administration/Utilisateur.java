f3d8b9085c1eaeb1d40be8a3c867e783cabcb26f
lea <lea-glandus@etud.univ-tln.fr>
2021-05-07 14:58:24 +0200

    add class formulaire pour devenir superviseur dans le modele

--- Code/Java/Serveur/src/main/java/com/univtln/univTlnLPS/model/administration/Utilisateur.java
+++ Code/Java/Serveur/src/main/java/com/univtln/univTlnLPS/model/administration/Utilisateur.java
@@ -51,6 +51,10 @@ public class Utilisateur implements SimpleEntity {
     @OneToOne
     private ScanData lastScan;
 
+    @XmlElement
+    @OneToOne
+    private FormDevenirSuper form;
+
 
     public enum Role {
         /**
