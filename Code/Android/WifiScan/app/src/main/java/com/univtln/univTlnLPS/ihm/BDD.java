78e71db5dfceed3e2ac2fe0535a62067f605e822
CharlesB83 <charlybiancheri99@gmail.com>
2021-05-20 15:42:04 +0200

    ajout bouton reinitialisation BDD (validé) + changement des couleurs

--- Code/Android/WifiScan/app/src/main/java/com/univtln/univTlnLPS/ihm/BDD.java
+++ Code/Android/WifiScan/app/src/main/java/com/univtln/univTlnLPS/ihm/BDD.java
@@ -1,14 +1,19 @@
 package com.univtln.univTlnLPS.ihm;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.univtln.univTlnLPS.R;
 import com.univtln.univTlnLPS.client.SSGBDControleur;
+
+import org.json.JSONException;
 
 public class BDD extends AppCompatActivity {
 
@@ -56,6 +61,46 @@ public class BDD extends AppCompatActivity {
         i.putExtra("ssgbdC", ssgbdControleur);
         startActivity(i);
 
+    }
+
+    public void flush(View v) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle("/!\\ ATTENTION /!\\");
+        builder.setMessage("Voulez-vous vraiment REINITIALISER la BDD");
+        builder.setPositiveButton("Confirmer",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        new Thread(new Runnable() {
+                            @Override
+                            public void run() {
+                                try {
+                                    ssgbdControleur.doRequest("PUT", "flush", null, true);
+                                    BDD.this.runOnUiThread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            Toast.makeText(BDD.this,
+                                                    "La base de données à bien été réinitialisée",
+                                                    Toast.LENGTH_LONG).show();
+                                            BDD.this.finish();
+                                        }
+                                    });
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }).start();
+                    }
+                });
+        builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 
 }
