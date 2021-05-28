a54e3c66a16eb28712b78107b9ddf80b6ac41876
lea <lea-glandus@etud.univ-tln.fr>
2021-05-28 14:21:56 +0200

    fix sonar's codeSmell

--- Code/Java/Serveur/sonar.sh
+++ Code/Java/Serveur/sonar.sh
@@ -1,1 +1,5 @@
-mvn sonar:sonar -D sonar.branch.name="$(git rev-parse --abbrev-ref HEAD|tr / _ )" -DskipTests=true -Dsonar.language=java -Dsonar.report.export.path=sonar-report.json -Dsonar.host.url=http://localhost:9000 --activate-profiles sonar
+mvn clean install
+mvn sonar:sonar  \
+	-D sonar.branch.name="$(git rev-parse --abbrev-ref HEAD|tr / _ )" \
+	-DskipTests=true \
+	--activate-profiles sonar
